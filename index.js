c4901b83b5d20e96d3a1e43ff43deec2ce659027
Kai Ninomiya <kainino1@gmail.com>
2022-01-31 15:38:19 -0800

    sync scroll

--- index.js
+++ index.js
@@ -4,23 +4,36 @@ class App extends Component {
   state = {
     font: 'textbook',
     text: 'a 糸　栈棧桟',
-  }
+  };
 
   setFont = ev => {
     if (ev.target.checked) this.setState({ font: ev.target.value });
     return false;
-  }
+  };
 
   setText = ev => {
     this.setState({ text: ev.target.value });
-  }
+  };
+
+  syncing = false;
+  syncScroll = ev => {
+    if (this.syncing) return;
+    this.syncing = true;
+    for (const el of document.querySelectorAll('.txtline')) {
+      if (el === ev.target) continue;
+      el.scrollLeft = ev.target.scrollLeft / ev.target.scrollWidth * el.scrollWidth;
+    }
+    setTimeout(() => { this.syncing = false; }, 10);
+  };
 
   render() {
     return html`
       <label><input type=radio name=font onchange=${this.setFont} checked=${this.state.font == 'textbook'}  value=textbook  />textbook</label>
       <label><input type=radio name=font onchange=${this.setFont} checked=${this.state.font == 'workbook'}  value=workbook  />workbook</label>
+      <br />
       <label><input type=radio name=font onchange=${this.setFont} checked=${this.state.font == 'notoserif'} value=notoserif />Noto Serif</label>
       <label><input type=radio name=font onchange=${this.setFont} checked=${this.state.font == 'sysserif'}  value=sysserif  />serif</label>
+      <br />
       <label><input type=radio name=font onchange=${this.setFont} checked=${this.state.font == 'notosans'}  value=notosans  />Noto Sans</label>
       <label><input type=radio name=font onchange=${this.setFont} checked=${this.state.font == 'syssans'}   value=syssans   />sans-serif</label>
       <div id=txt class="${this.state.font}">
@@ -28,21 +41,21 @@ class App extends Component {
           <div class=langlabel>zh-hans</div>
           <span lang=zh-hans>${this.state.text}</span>
           <br />
-          <input lang=zh-hans type=text value="${this.state.text}" oninput=${this.setText} />
+          <input type=text class=txtline value="${this.state.text}" oninput=${this.setText} onscroll=${this.syncScroll} lang=zh-hans />
         </label>
         <br />
         <label>
           <div class=langlabel>zh-hant</div>
           <span lang=zh-hant>${this.state.text}</span>
           <br />
-          <input lang=zh-hant type=text value="${this.state.text}" oninput=${this.setText} />
+          <input type=text class=txtline value="${this.state.text}" oninput=${this.setText} onscroll=${this.syncScroll} lang=zh-hant />
         </label>
         <br />
         <label>
           <div class=langlabel>ja</div>
           <span lang=ja>${this.state.text}</span>
           <br />
-          <input lang=ja type=text value="${this.state.text}" oninput=${this.setText} />
+          <input type=text class=txtline value="${this.state.text}" oninput=${this.setText} onscroll=${this.syncScroll} lang=ja />
         </label>
       </div>
     `;
